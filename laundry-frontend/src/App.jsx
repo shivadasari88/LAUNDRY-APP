070e00f19832f0fa45bd5fbab2ebfdae8f73855d
SHIVA SAI RAMA KRISHNA DASARI <144298375+shivadasari88@users.noreply.github.com>
2025-12-18 22:51:26 +0530

    Revert "My changes on new branch"

--- laundry-frontend/src/App.jsx
+++ laundry-frontend/src/App.jsx
@@ -1,31 +1,25 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Layout from './components/layout/Layout';
-import { CartProvider } from './context/CartContext';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Landing from "./pages/Landing";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import CustomerDashboard from "./pages/CustomerDashboard";
+import ProviderDashboard from "./pages/ProviderDashboard";
+import AdminDashboard from "./pages/AdminDashboard";
 
-// Pages
-import Dashboard from './pages/Dashboard';
-import Services from './pages/Services';
-import Cart from './pages/Cart';
-import MyOrders from './pages/MyOrders';
-import Profile from './pages/Profile';
-
-function App() {
+export default function App() {
   return (
     <BrowserRouter>
-      <CartProvider>
-        <Layout>
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/services" element={<Services />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/orders" element={<MyOrders />} />
-            <Route path="/profile" element={<Profile />} />
-          </Routes>
-        </Layout>
-      </CartProvider>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+       <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+
+       
+        <Route path="/customer" element={<CustomerDashboard />} />
+        <Route path="/provider" element={<ProviderDashboard />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+        
+      </Routes>
     </BrowserRouter>
   );
 }
-
-export default App;
