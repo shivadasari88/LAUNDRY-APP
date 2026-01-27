1fbc83d14ea6fedd610b2d6aa8ed432b2f0ce4d1
samruddhi614 <samruddhichetule0@gmail.com>
2026-01-27 13:07:49 +0530

    APPROVEDSHOP AND REJECTECHSHOP COMPONENTS ARE CREATED

--- laundry-frontend/src/App.jsx
+++ laundry-frontend/src/App.jsx
@@ -15,6 +15,8 @@ import Home from "./pages/customer/Home";
 import Shop from "./pages/customer/Shop";
 
 import { CartProvider } from "./context/CartContext";
+import ApprovedShops from "./pages/admin/ApprovedShops";
+import RejectedShops from "./pages/admin/RejectedShops";
 
 export default function App() {
   return (
@@ -39,6 +41,9 @@ export default function App() {
 
           {/* Admin */}
           <Route path="/admin" element={<AdminDashboard />} />
+          <Route path="/admin/approvedShops" element={<ApprovedShops/>} />
+          <Route path="/admin/rejectedShops" element={<RejectedShops/>} />
+
         </Routes>
       </BrowserRouter>
     </CartProvider>
--- laundry-frontend/src/pages/admin/AdminDashboard.jsx
+++ laundry-frontend/src/pages/admin/AdminDashboard.jsx
@@ -102,3 +102,5 @@ const AdminDashboard = () => {
 };
 
 export default AdminDashboard;
+
+
